846f6ed9b3d3cb6be7debcd79acdd36af9528316
Jakub <kubastr98@gmail.com>
2021-01-13 20:22:36 +0100

    test

--- annealing_test.py
+++ annealing_test.py
@@ -1,6 +1,20 @@
-import numpy
 import matplotlib.pyplot as plt
-import annealing_3
+from annealing_3 import Solver1
+
+
+def plot_costtemp (temperature, cost_function):
+    plt.title('Wartość funkcji kosztu w zależności od temperatury')
+    plt.xlabel('Temperatura')
+    plt.ylabel('Wartość funkcji kosztu')
+    plt.xlim(temperature[0], temperature[-1])
+    plt.plot(temperature, cost_function)
+    plt.show()
 
 
 
+solver = Solver1()
+solver.simulated_annealing()
+
+print(solver.temp_results)
+
+plot_costtemp(solver.temp_results, solver.cost_function_results)
