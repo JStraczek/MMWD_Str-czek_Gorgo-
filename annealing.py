c9ed68c04f67b6d2cb16587fdb9e6265ffc0523d
unknown <kubastr98@gmail.com>
2021-01-16 16:33:42 +0100

    final version

--- annealing.py
+++ annealing.py
@@ -30,49 +30,57 @@ class Order():
       
 class Solver:
 
+    cost_matrix = np.array(
+
+    [[	inf	,	3	,	10	,	6	,	2	,	7	,	3	,	10	,	9	,	4	,	2	,	3	,	10	,	9	,	8	,	10	,	4	,	6	,	5	,	6	],
+    [	inf	,	inf	,	10	,	6	,	5	,	6	,	1	,	8	,	1	,	4	,	3	,	4	,	1	,	5	,	2	,	3	,	5	,	8	,	9	,	1	],
+    [	10	,	10	,	inf	,	3	,	4	,	7	,	7	,	7	,	6	,	2	,	10	,	1	,	6	,	7	,	4	,	6	,	1	,	5	,	2	,	3	],
+    [	6	,	6	,	inf	,	inf	,	7	,	5	,	7	,	1	,	5	,	9	,	4	,	8	,	7	,	9	,	8	,	10	,	9	,	1	,	6	,	5	],
+    [	2	,	5	,	4	,	7	,	inf	,	10	,	10	,	6	,	3	,	9	,	7	,	2	,	8	,	3	,	5	,	8	,	6	,	5	,	4	,	9	],
+    [	7	,	6	,	7	,	5	,	inf	,	inf	,	10	,	7	,	3	,	8	,	6	,	8	,	8	,	3	,	8	,	6	,	9	,	4	,	8	,	2	],
+    [	3	,	1	,	7	,	7	,	10	,	10	,	inf	,	5	,	1	,	1	,	7	,	10	,	1	,	4	,	9	,	7	,	3	,	2	,	1	,	1	],
+    [	10	,	8	,	7	,	1	,	6	,	7	,	inf	,	inf	,	7	,	1	,	1	,	5	,	4	,	2	,	9	,	4	,	4	,	5	,	10	,	2	],
+    [	9	,	1	,	6	,	5	,	3	,	3	,	1	,	7	,	inf	,	8	,	9	,	4	,	1	,	7	,	4	,	10	,	6	,	8	,	6	,	7	],
+    [	4	,	4	,	2	,	9	,	9	,	8	,	1	,	1	,	inf	,	inf	,	10	,	10	,	7	,	10	,	6	,	4	,	1	,	7	,	8	,	10	],
+    [	2	,	3	,	10	,	4	,	7	,	6	,	7	,	1	,	9	,	10	,	inf	,	6	,	8	,	1	,	5	,	9	,	2	,	3	,	3	,	7	],
+    [	3	,	4	,	1	,	8	,	2	,	8	,	10	,	5	,	4	,	10	,	inf	,	inf	,	5	,	7	,	9	,	9	,	9	,	6	,	6	,	5	],
+    [	10	,	1	,	6	,	7	,	8	,	8	,	1	,	4	,	1	,	7	,	8	,	5	,	inf	,	10	,	3	,	9	,	7	,	6	,	2	,	8	],
+    [	9	,	5	,	7	,	9	,	3	,	3	,	4	,	2	,	7	,	10	,	1	,	7	,	inf	,	inf	,	9	,	7	,	9	,	8	,	2	,	4	],
+    [	8	,	2	,	4	,	8	,	5	,	8	,	9	,	9	,	4	,	6	,	5	,	9	,	3	,	9	,	inf	,	5	,	1	,	2	,	8	,	4	],
+    [	10	,	3	,	6	,	10	,	8	,	6	,	7	,	4	,	10	,	4	,	9	,	9	,	9	,	7	,	inf	,	inf	,	9	,	3	,	3	,	1	],
+    [	4	,	5	,	1	,	9	,	6	,	9	,	3	,	4	,	6	,	1	,	2	,	9	,	7	,	9	,	1	,	9	,	inf	,	10	,	4	,	8	],
+    [	6	,	8	,	5	,	1	,	5	,	4	,	2	,	5	,	8	,	7	,	3	,	6	,	6	,	8	,	2	,	3	,	inf	,	inf	,	7	,	8	],
+    [	5	,	9	,	2	,	6	,	4	,	8	,	1	,	10	,	6	,	8	,	3	,	6	,	2	,	2	,	8	,	3	,	4	,	7	,	inf	,	6	],
+    [	6	,	1	,	3	,	5	,	9	,	2	,	1	,	2	,	7	,	10	,	7	,	5	,	8	,	4	,	4	,	1	,	8	,	8	,	inf	,	inf	]]
+            ) 
+
     T0 = 10000 # initial temperature
     Tmin = 1 # minimal temperature
     k = 10 # number of iteration per era
     alpha = 0.8 # cooling coefficient
-    max_prohibited_solutions=300
+
+    order_amount = 10
+    max_prohibited_solutions=500
+    max_time = order_amount * 10
+    scaler = max_time / 50
+
+    penalties_matrix= np.array(
+                [[0*scaler, 10*scaler, 20],
+                [10*scaler, 20*scaler, 10],
+                [20*scaler, 30*scaler, 5],
+                [30*scaler, 40*scaler, 0],
+                [40*scaler, 50*scaler, -10],
+                [50*scaler, inf, -inf]], dtype=object) 
     
     backpack_volume=3 # backpack volume
-    restaurants = [Order.A, Order.B, Order.C, Order.D, Order.E] #points of collect
-    customers = [Order.a, Order.b, Order.c, Order.d, Order.e] #points of deliver
+    restaurants = [Order.A, Order.B, Order.C, Order.D, Order.E, Order.F, Order.G, Order.H, Order.I, Order.J] #points of collect
+    customers = [Order.a, Order.b, Order.c, Order.d, Order.e, Order.f, Order.g, Order.h, Order.i, Order.j] #points of deliver
+
+    restaurants = restaurants[0:order_amount]
+    customers = customers[0:order_amount]
 
     cost_function_out = []
-    
-    cost_matrix = np.array(
 
-[[	inf	,	3	,	8	,	3	,	9	,	4	,	1	,	10	,	10	,	2	,	4	,	4	,	6	,	8	,	2	,	2	,	6	,	6	,	7	,	3	],
-[	inf	,	inf	,	10	,	9	,	9	,	1	,	1	,	8	,	1	,	4	,	3	,	4	,	1	,	5	,	2	,	3	,	5	,	8	,	9	,	1	],
-[	10	,	10	,	inf	,	3	,	4	,	7	,	7	,	7	,	6	,	2	,	10	,	1	,	6	,	7	,	4	,	6	,	1	,	5	,	2	,	3	],
-[	6	,	6	,	inf	,	inf	,	7	,	5	,	7	,	1	,	5	,	9	,	4	,	8	,	7	,	9	,	8	,	10	,	9	,	1	,	6	,	5	],
-[	2	,	5	,	9	,	2	,	inf	,	10	,	10	,	6	,	3	,	9	,	7	,	2	,	8	,	3	,	5	,	8	,	6	,	5	,	4	,	9	],
-[	7	,	6	,	8	,	1	,	inf	,	inf	,	10	,	7	,	3	,	8	,	6	,	8	,	8	,	3	,	8	,	6	,	9	,	4	,	8	,	2	],
-[	3	,	4	,	1	,	4	,	7	,	7	,	inf	,	5	,	1	,	1	,	7	,	10	,	1	,	4	,	9	,	7	,	3	,	2	,	1	,	1	],
-[	10	,	3	,	8	,	2	,	6	,	1	,	inf	,	inf	,	7	,	1	,	1	,	5	,	4	,	2	,	9	,	4	,	4	,	5	,	10	,	2	],
-[	9	,	4	,	1	,	5	,	3	,	2	,	7	,	10	,	inf	,	8	,	9	,	4	,	1	,	7	,	4	,	10	,	6	,	8	,	6	,	7	],
-[	4	,	1	,	8	,	8	,	1	,	4	,	6	,	10	,	inf	,	inf	,	10	,	10	,	7	,	10	,	6	,	4	,	1	,	7	,	8	,	10	],
-[	2	,	3	,	6	,	1	,	9	,	4	,	7	,	6	,	5	,	9	,	inf	,	6	,	8	,	1	,	5	,	9	,	2	,	3	,	3	,	7	],
-[	3	,	9	,	7	,	2	,	9	,	2	,	8	,	2	,	7	,	5	,	inf	,	inf	,	5	,	7	,	9	,	9	,	9	,	6	,	6	,	5	],
-[	10	,	8	,	1	,	10	,	5	,	7	,	2	,	3	,	7	,	1	,	3	,	6	,	inf	,	10	,	3	,	9	,	7	,	6	,	2	,	8	],
-[	9	,	4	,	4	,	3	,	3	,	8	,	10	,	4	,	3	,	7	,	8	,	3	,	inf	,	inf	,	9	,	7	,	9	,	8	,	2	,	4	],
-[	8	,	7	,	8	,	6	,	3	,	9	,	8	,	6	,	4	,	1	,	5	,	10	,	1	,	10	,	inf	,	5	,	1	,	2	,	8	,	4	],
-[	10	,	7	,	4	,	5	,	1	,	5	,	3	,	6	,	5	,	4	,	10	,	4	,	2	,	4	,	inf	,	inf	,	9	,	3	,	3	,	1	],
-[	4	,	7	,	5	,	6	,	9	,	10	,	10	,	9	,	8	,	3	,	6	,	9	,	1	,	8	,	7	,	6	,	inf	,	10	,	4	,	8	],
-[	6	,	2	,	7	,	1	,	8	,	10	,	2	,	6	,	8	,	6	,	9	,	5	,	7	,	10	,	4	,	8	,	inf	,	inf	,	7	,	8	],
-[	5	,	7	,	7	,	7	,	8	,	6	,	6	,	8	,	2	,	2	,	8	,	3	,	10	,	5	,	8	,	8	,	8	,	3	,	inf	,	6	],
-[	6	,	8	,	8	,	10	,	5	,	7	,	4	,	7	,	3	,	9	,	9	,	7	,	7	,	1	,	9	,	7	,	4	,	8	,	inf	,	inf	]]
-        ) #bardziej wlasciwa nazwa to time matrix
-    
-    penalties_matrix= np.array(
-                   [[0, 10, 10],
-                    [10, 20, 5],
-                    [20, 30, 0],
-                    [30, 40, -10],
-                    [40, 50, -25],
-                    [50, inf, -1000]], dtype=object) #bardziej wlasciwa nazwa to cost matrix
     
     def simulated_annealing(self):
         T = self.T0
@@ -97,7 +105,6 @@ class Solver:
                     limiter=0
                     if limiter==self.max_prohibited_solutions:
                         print("program nie moze osiagnac dopuszczalnego rozwiazana")
-                        return None
                     else:
                         limiter+=1
 
@@ -145,15 +152,26 @@ class Solver:
                 backpack.remove(backpack[next_point])
         return x_init
     
-    def check_solution(self, route):
+    def check_solution(self, route): 
+        if route[0][1].islower():
+            return False
+        else: 
+            tmp = [route[0]]
+
+        for el in route[1:]:
+            if el[1].islower() and (el[0] - 1, el[1].upper()) not in tmp:
+                return False
+            elif el[1].isupper():
+                tmp.append(el)
+
         time=0
         prev_point=route[0]
         for point in route[1:]:
             time+=self.cost_matrix[prev_point[0],point[0]]
             prev_point=point
 
-        if time>50:
-            return False #prohibited_soultion
+        if time>self.max_time:
+            return False #prohibited_solution
 
         return True
     
--- annealing_test.py
+++ annealing_test.py
@@ -1,16 +1,17 @@
 from annealing import Solver
 
 
-# Plotted tests
+# # Plotted tests
 solver = Solver()
 solver.plot_costepoch()
 
 
 # Loop test
 best_solution=solver.simulated_annealing()
+print("iteration number ",0, "route ",best_solution[0],'tip= ',best_solution[1])
 for i in range(10):
     curr_solution=solver.simulated_annealing()
-    print("iteration number ",i, "route ",curr_solution[0],'tip= ',curr_solution[1])
+    print("iteration number ",i+1, "route ",curr_solution[0],'tip= ',curr_solution[1])
     if best_solution[1]<curr_solution[1]:
         best_solution=curr_solution
 print("Best solution, route: ",best_solution[0],'tip= ',best_solution[1])
